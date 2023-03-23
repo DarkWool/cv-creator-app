729c02ee468e1c4a0357610afbf717a4a635dbc8
DarkWool <cooper27@live.com.mx>
2023-03-22 23:38:48 -0600

    Add placeholder prop to Textarea component

--- src/components/FormElements/Textarea.js
+++ src/components/FormElements/Textarea.js
@@ -1,10 +1,11 @@
-export function Textarea({ label, id, value, onInputChange, inputClass, rows }) {
+export function Textarea({ label, id, value, onInputChange, inputClass, rows, placeholder }) {
   return (
     <>
       {label && <label htmlFor={id}>{label}</label>}
       <textarea
         id={id}
         name={id}
+        placeholder={placeholder}
         value={value}
         onChange={onInputChange}
         className={inputClass}
